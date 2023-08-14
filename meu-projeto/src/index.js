92d816b81d22a0c5cff476df856ed693564387b2
André Rodrigues <109726109+AndreRodrigues1991@users.noreply.github.com>
2023-08-14 03:45:40 -0300

    pdate

--- meu-projeto/src/index.js
+++ meu-projeto/src/index.js
@@ -14,5 +14,5 @@ root.render(
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-// https://bit.ly/CRA-vitals 
+// quando precisamos usar uma variavel com valores diferente dentro primeiro adicionamos
 reportWebVitals();
